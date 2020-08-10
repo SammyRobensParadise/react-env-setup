22b23e66098f3ca9741be4d5eee59da6da7e444c
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-08-09 21:06:56 -0700

    changes to handle colored text

--- create.sh
+++ create.sh
@@ -8,11 +8,11 @@ NC='\033[0m'
 #helper functions
 helpFunction()
 {
-   echo ""
-   echo "Usage: $0 -a gitUsername -b projectName -c withFirebase"
-   echo -e "\t-a Description of what is gitUsername"
-   echo -e "\t-b Description of what is projectName"
-   echo -e "\t-c Description of what is withFirebase"
+   echo -e ""
+   echo -e "Usage: $0 -a gitUsername -b projectName -c withFirebase"
+   echo -e -e "\t-a Description of what is gitUsername"
+   echo -e -e "\t-b Description of what is projectName"
+   echo -e -e "\t-c Description of what is withFirebase"
    exit 1 # Exit script after printing help
 }
 
@@ -34,25 +34,28 @@ then
 fi
 
 # Begin script in case all parameters are correct
-echo "${PURPLE}Beginning Script...."
-echo "${GREEN}Git Username: $gitUsername"
-echo "Project Name $projectName"
-echo "With Firebase: $withFirebase"
-echo "${PURPLE}Making Project in parent directory using create-react-app..."
+echo -e "${PURPLE}Beginning Script...."
+echo -e "${GREEN}Git Username: $gitUsername"
+echo -e "Project Name $projectName"
+echo -e "With Firebase: $withFirebase"
+echo -e "${PURPLE}Making Project in parent directory using create-react-app..."
 cd ../
+npm uninstall -g create-react-app
 npx create-react-app $projectName
 cd ./$projectName
-echo "Initializing repository as git repository"
+echo -e "Initializing repository as git repository"
 git init
-echo "Updating npm..."
+echo -e "Updating npm..."
 npm -v
 npm i npm
 npm -v
-echo "Installing npm dependencies"
+echo -e "Installing npm dependencies"
 npm install @loadable/component react-cookie react-lazy-load-image-component react-lazyload @material-ui/core react-redux react-router react-router-dom react-scripts react-scroll styled-components
-echo "${GREEN}Sucessfully installed dependencies"
-echo "committing and pushing to a remote repository"
+npm audit fix
+echo -e "${GREEN}Sucessfully installed dependencies"
+echo -e "committing and pushing to a remote repository"
 git add .
 git commit -m "Initial commit to repository"
 git remote add origin https://github.com/$gitUsername/$projectName.git
 git push origin master
+echo -e "done"
